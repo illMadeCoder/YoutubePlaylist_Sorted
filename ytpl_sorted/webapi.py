f00fc048072211c6b8154414a1e1ffd438623245
illMadeCoder <illMadeCoder@gmail.com>
2022-01-23 17:53:38 -0500

    PORT as config variable

--- ytpl_sorted/webapi.py
+++ ytpl_sorted/webapi.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 from ytplsorted import ytplsorted
+from config import PORT
 app = Flask("ytplsorted")
 
 # program(argsservice.playlistIDs, 
@@ -19,4 +20,4 @@ def ytplsortedEndpoint():
     return redirect(url, code=302)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=config.PORT)
